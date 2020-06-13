ecb24e0b8b97d24b971c782378cda658e3d04062
michael garrison <mgarrison.sa@gmail.com>
2020-06-13 17:18:16 -0500

    arrays basics exercise complete

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,7 +1,31 @@
 import java.util.Arrays;
 
 public class ArraysExercises {
+    public static Person[] addPerson(Person[] family, Person person){
+        Person[] oneMoreFam = Arrays.copyOf(family, family.length + 1);
+        oneMoreFam[oneMoreFam.length - 1] = person;
+        return oneMoreFam;
+    }
+
     public static void main(String[] args) {
+
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(numbers);
+//        System.out.println(Arrays.toString(numbers));
+
+        Person[] family = new Person[3];
+        family[0] = new Person("michael");
+        family[1] = new Person("trey");
+        family[2] = new Person("amanda");
+
+        family = addPerson(family, new Person("molly"));
+        for (Person fam: family){
+            System.out.println(fam.getName());
+        }
+
+
+
+
 
     }
 
