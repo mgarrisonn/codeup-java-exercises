ee21177657c7a122294b13eb6407e5d4ead6cc85
michael garrison <mgarrison.sa@gmail.com>
2020-06-15 15:01:54 -0500

    inheritance mini exercise 2

--- src/Car.java
+++ src/Car.java
@@ -1,3 +1,32 @@
 public class Car extends Vehicle {
 
+    private int milesPerGallon;
+
+    public Car(boolean isOperable, int passengerCapacity, String powerSource, int milesPerGallon){
+        super(isOperable, passengerCapacity, powerSource);
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    public int getMilesPerGallon(){
+        return milesPerGallon;
+    }
+
+    public void setMilesPerGallon(int milesPerGallon) {
+        this.milesPerGallon = milesPerGallon;
+    }
+
+    public void deployAirBag(){
+        System.out.println("Deploying airbag...");
+    }
+
+    public void deployingAirBag(String air){
+        System.out.println(air);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "milesPerGallon=" + milesPerGallon +
+                '}';
+    }
 }
--- src/Jet.java
+++ src/Jet.java
@@ -1,3 +1,32 @@
 public class Jet extends Vehicle {
 
+    private int maximumAltitude;
+
+    public Jet(boolean isOperable, int passengerCapacity, String powerSource, int maximumAltitude){
+        super(isOperable, passengerCapacity, powerSource);
+        this.maximumAltitude = maximumAltitude;
+    }
+
+    public int getMaximumAltitude() {
+        return maximumAltitude;
+    }
+
+    public void setMaximumAltitude(int maximumAltitude) {
+        this.maximumAltitude = maximumAltitude;
+    }
+
+    public void takeOff(){
+        System.out.println("Taking off...");
+    }
+
+    public void takeOff(String take){
+        System.out.println(take);
+    }
+
+    @Override
+    public String toString() {
+        return "Jet{" +
+                "maximumAltitude=" + maximumAltitude +
+                '}';
+    }
 }
