288e97c933f42be494f362f33107b91744cd1257
michael garrison <mgarrison.sa@gmail.com>
2020-06-16 11:43:56 -0500

    completed shapes exercise using inheritance and polymorphism

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,4 +1,21 @@
 package shapes;
 
-public class Square {
+public class Square extends Rectangle{
+
+
+    public Square(double side){
+        super(side, side);
+    }
+
+    @Override
+    public double getArea(){
+        System.out.println("overridden in square");
+        return Math.pow(this.length, 2);
+    }
+
+    @Override
+    public double getPerimeter(){
+        return 4 * this.length;
+    }
+
 }
