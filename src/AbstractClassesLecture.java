e8e6234785cad740495380da0139d2687289a9b2
michael garrison <mgarrison.sa@gmail.com>
2020-06-17 12:32:55 -0500

    slide notes for Abstract classes and interfaces

---
+++ src/AbstractClassesLecture.java
@@ -0,0 +1,19 @@
+//public class AbstractClassesLecture {
+
+//  Interfaces and Abstract Classes use the concepts of inheritance and polymorphism but with slightly different specifications
+//  Abstract Classes: Act as a superclass however -
+//  - it cannot be directly instantiated
+//  - Abstract methods don't have their own implementations
+//  - Can provide core functionality, while subclass provides minor variations
+//  - The subclass must "fill in the blanks"
+//  - Have implementations for all abstract methods that come from the Abstract class
+
+
+//  Interfaces are a special type of Abstract class, but with even more specific rules
+//  - All properties are public, static and final
+//  - All methods are public and abstract
+//  - No methods are static
+//  - Typically implemented to define class constants and define how the object will interact with the application
+//  - Allows us to write code that is not dependant on implementation
+
+
