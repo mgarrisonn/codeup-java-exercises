7b3a051ab711cb8411a0725c72efa86e0e1726e0
michael garrison <mgarrison.sa@gmail.com>
2020-06-18 22:23:44 -0500

    collections exercise question 1 and 2

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -1,4 +1,36 @@
 package grades;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class GradesApplication {
+
+    public static void main(String[] args) {
+        HashMap<String, Student> students = new HashMap<>()>;
+        Student s1 = new Student("Michael");
+        s1.studentGrades(92);
+        s1.studentGrades(74);
+        s1.studentGrades(73);
+        Student s2 = new Student("Jason");
+        s2.studentGrades(87);
+        s2.studentGrades(81);
+        s2.studentGrades(98);
+        Student s3 = new Student("Kevin");
+        s3.studentGrades(64);
+        s3.studentGrades(72);
+        s3.studentGrades(91);
+        Student s4 = new Student("Trey");
+        s4.studentGrades(71);
+        s4.studentGrades(79);
+        s4.studentGrades(93);
+
+        students.put("michael123", s1);
+        students.put("Jason123", s2);
+        students.put("Kevin123", s3);
+        students.put("Trey123", s4);
+
+
+
+    }
+
 }
--- src/grades/Student.java
+++ src/grades/Student.java
@@ -1,4 +1,43 @@
 package grades;
 
+import java.util.ArrayList;
+
 public class Student {
+
+//    public static void main(String[] args) {
+//        Student s1 = Student("Michael");
+//    }
+
+     private String name;
+     private ArrayList<Integer> grades;
+
+    public Student(String name){
+        this.name = name;
+        this.grades = new ArrayList<>();
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void studentGrades(int grade){
+        grades.add(grade);
+    }
+
+    public double studentGradeAverage(){
+        int average = 0;
+        for(int i = 0; i < grades.size(); i++){
+            average += grades.get(i);
+        }
+        return average / grades.size();
+    }
+
+    public static void main(String[] args) {
+        Student s1 = new Student("Michael");
+        s1.studentGrades(90);
+        s1.studentGrades(78);
+        System.out.println(s1.studentGradeAverage());
+    }
+
+
 }
