fff5e865f0c8d93f26ab8c2dd3437dc19e82ccde
michael garrison <mgarrison.sa@gmail.com>
2020-07-14 22:49:57 -0500

    refactored

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,26 +4,26 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main (String[] args) {
 
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f.", pi);
-//
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f.", pi);
+
         Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter an integer: ");
-//        int userInt = sc.nextInt();
-//        System.out.println("The user entered: " + userInt);
-//
-//        System.out.println("Please enter three words...");
-//        String word1 = sc.next();
-//        String word2 = sc.next();
-//        String word3 = sc.next();
-//        System.out.println(word1);
-//        System.out.println(word2);
-//        System.out.println(word3);
-//        System.out.printf("%s%n%s%n%S%n", word1, word2, word3);
-//
-//        System.out.println("Please enter a sentence...");
-//        String sentence = sc.nextLine();
-//        System.out.println(sentence);
+        System.out.println("Enter an integer: ");
+        int userInt = sc.nextInt();
+        System.out.println("The user entered: " + userInt);
+
+        System.out.println("Please enter three words...");
+        String word1 = sc.next();
+        String word2 = sc.next();
+        String word3 = sc.next();
+        System.out.println(word1);
+        System.out.println(word2);
+        System.out.println(word3);
+        System.out.printf("%s%n%s%n%S%n", word1, word2, word3);
+
+        System.out.println("Please enter a sentence...");
+        String sentence = sc.nextLine();
+        System.out.println(sentence);
 
         System.out.println("Please enter length of room in centimeters:");
         String length = sc.nextLine();
